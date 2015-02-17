94d2af96a7a722e1a4a3682185fa54f2b5d2f24f
Kat Passen <kazagorski@gmail.com>
2015-02-16 21:42:27 -0500

    Add variable length of output via command-line args
    Todo: decide if the outfile should be updated destructively or not, if
    there even should be one

--- markov_gen.py
+++ markov_gen.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
+import argparse
 from pymarkovchain import MarkovChain
 
 mc = MarkovChain('./kithrixx_ebooks')
+parser = argparse.ArgumentParser()
+parser.add_argument("reps", help="number of strings generated", type=int)
+args = parser.parse_args()
 
 ebooks_fh = open('./ebooks.txt', 'w')
 
-for i in range(15):
+for i in range(args.reps):
 	ebooks_fh.write(mc.generateString() + '\n')
